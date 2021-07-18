ad7ce8811fae1bce85db20a800b9fae6d4945606
suhem <suhemparashar@gmail.com>
2021-07-18 12:03:49 +0530

    Database Setup I

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+
+app.get('/', (req, res) =>{
+    res.send('API is Running...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+
+app.get('/api/products/:id', (req, res) => {
+    res.json(products.find(p => p._id === req.params.id))
+})
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
